0a9190f2217f1f99957970d6df8e2ae248f0c6f4
Saad <128216930+elm-Saad@users.noreply.github.com>
2023-07-25 15:45:06 +0200

    update

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const listOfMessagesDb = ref(Db, "messagesList")
 
 
 const textInputEl = document.getElementById('input-text-field');
-//const toUser = document.getElementById('input-text-field');
-//const fromUser = document.getElementById('input-text-field');
+const toUser = document.getElementById('input-el-to');
+const fromUser = document.getElementById('input-el-from');
 const publishBtn = document.getElementById('publish-btn');
 const displayField = document.getElementById('display-inputs');
 
@@ -20,21 +20,19 @@ const displayField = document.getElementById('display-inputs');
 // push into the DB 
 publishBtn.addEventListener('click', function(){
     let userInputValue = textInputEl.value;
-    //let toUserValue = toUser.value;
-    //let fromUserValue = fromUser.value;
+    let toUserValue = toUser.value;
+    let fromUserValue = fromUser.value;
 
-    //if(userInputValue /*&& toUserValue && fromUserValue*/) {
-    //let GlobaleMessage = `${toUserValue} , 
-    //                      ${userInputValue} 
-    //                ${fromUserValue} `;
-    push(listOfMessagesDb,userInputValue);
+    if(userInputValue && toUserValue && fromUserValue){
+        let GlobaleMessage ={
+            mainMessage:userInputValue,
+            To:toUserValue,
+            from:fromUserValue
+        }
+        push(listOfMessagesDb,GlobaleMessage);
 
-    clearInputFieldEl();
-
-    //}
-   // else{
-       // alert('you should field all the inputs in order to share your message to the world');
-   // }
+        clearInputFieldEl();
+    }
 });
 
 //get the DB info 
